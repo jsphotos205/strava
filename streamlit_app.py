995fbb008af70ebef666afa77a1977d3cf9b1335
John Sloan <jsloanphoto@gmail.com>
2023-07-24 18:26:43 -0400

    streamlit reorder_columns for df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,32 @@
 import streamlit as st
 import pandas as pd
 import os
-
+import seaborn as sns
 
 def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
+
+def reorder_columns(df):
+    column_order = ['start_date',
+                    'name',
+                    'moving_time_minutes',
+                    'distance_miles',
+                    'average_speed',
+                    'max_speed',
+                    'total_elevation_gain',
+                    'elev_high',
+                    'elev_low',
+                    'pr_count',
+                    'tavg',
+                    'tmin',
+                    'tmax',
+                    'prcp',
+                    'snow',
+                    'wdir',
+                    'wspd',
+                    'pres']
+    return df[column_order]
 
 def main():
 
@@ -19,14 +40,15 @@ def main():
     if selected_file:
         file_path = os.path.join(data_dir, selected_file)
         run_data = load_data(file_path)
+        run_data = reorder_columns(df=run_data)
 
-        st.subheader('Run Previews')
+        st.subheader('List of Runs:')
+        # run_data = reorder_columns(run_data)
         st.dataframe(run_data)
-
-        st.subheader('Run Summaries')
         st.write(run_data.describe())
+        st.caption('Combined run/weather data for RRGCC runs')
 
-    st.title('Folium Map Viewer')
+    st.title('RRGCC Run Viewer')
 
     map_dir = 'maps/'
 
