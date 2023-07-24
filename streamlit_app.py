9b94fa0ccd744f40c422d5d8d05cdb349afc6f1c
John Sloan <jsloanphoto@gmail.com>
2023-07-24 15:42:20 -0400

    start on streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import os
+
+
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def main():
+
+    st.title('RRGCC Running Data')
+
+    data_dir = 'csv/run/rrgcc_loops'
+
+    csv_files = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
+    selected_file = st.selectbox('Select:', csv_files, index=0)
+
+    if selected_file:
+        file_path = os.path.join(data_dir, selected_file)
+        run_data = load_data(file_path)
+
+        st.subheader('Run Previews')
+        st.dataframe(run_data)
+
+        st.subheader('Run Summaries')
+        st.write(run_data.describe())
+
+    st.title('Folium Map Viewer')
+
+    map_dir = 'csv/maps/'
+
+    map_files = [f for f in os.listdir(map_dir) if f.endswith('html')]
+
+    selected_map = st.selectbox('Select a Map:', map_files, index=0)
+
+    if selected_map:
+        map_path = os.path.join(map_dir, selected_map)
+
+        with open(map_path, 'r') as f:
+            map_html = f.read()
+
+        st.components.v1.html(map_html, width=800, height=600)
+    
+
+if __name__ == '__main__':
+    main()
