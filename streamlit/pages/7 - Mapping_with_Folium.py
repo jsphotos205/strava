02da136455d36805e51d2d03af04ba3d775a0771
John Sloan <jsloanphoto@gmail.com>
2023-07-27 15:50:07 -0400

    bug fix maybe

--- streamlit/pages/7 - Mapping_with_Folium.py
+++ streamlit/pages/7 - Mapping_with_Folium.py
@@ -2,12 +2,12 @@ import streamlit as st
 from Home import *
 
 def main():
-    
+
     page_config = Page_Config()
     page_config.set_page_config()
 
     mapping_content = load_markdown_file('streamlit/pages/md/Mapping.md')
-    mapping_content
+    st.markdown(mapping_content, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
